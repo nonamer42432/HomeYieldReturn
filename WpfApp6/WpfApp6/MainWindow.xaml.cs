92a8eea41c5056d50194fd3bc0121388d4378714
Gamerofgtaonline <61492510+nonamer42432@users.noreply.github.com>
2024-12-01 11:18:06 +0500

    Add files via upload

---
+++ WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void GenerateNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(txtStart.Text, out int start) || !int.TryParse(txtEnd.Text, out int end))
+            {
+                MessageBox.Show("Введите допустимые целочисленные значения.");
+                return;
+            }
+
+            if (start > end)
+            {
+                MessageBox.Show("Начальное значение должно быть меньше или равно конечному значению.");
+                return;
+            }
+
+
+            lstNumbers.Items.Clear(); // Очищение предыдущего номера
+
+            NumberGenerator generator = new NumberGenerator();
+            await Task.Run(() =>
+            {
+                foreach (int number in generator.GenerateNumbers(start, end))
+                {
+                    this.Dispatcher.Invoke(() => lstNumbers.Items.Add(number)); // Обновлять пользовательский интерфейс в основном потоке
+                }
+            });
+
+            MessageBox.Show("Генерация завершена!");
+        }
+    }
+}
---
+++ WpfApp6/WpfApp6/NumberGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class NumberGenerator
+    {
+        public IEnumerable<int> GenerateNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                yield return i; // Медленно выдает число
+                System.Threading.Thread.Sleep(100); // Моделирует работу
+            }
+        }
+    }
+}
